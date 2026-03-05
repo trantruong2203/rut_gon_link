6ac4654ccf5669ed411182625e45a00e0316a4db
trantruong2203 <newhorizon6789@gmai.com>
2026-03-06 00:07:42 +0700

    Fix copy_templates to also copy Flash folder case-insensitive

--- copy_templates.php
+++ copy_templates.php
@@ -1,8 +1,17 @@
 <?php
+// Copy templates from plugins to lowercase folders
 foreach(glob("/var/www/adlinkfly/plugins/*/templates/*") as $d) {
     $lower = strtolower($d);
     if(!file_exists($lower) && is_dir($d)) {
         exec("cp -r \"$d\" \"$lower\"");
     }
+}
+
+// Copy element/Flash to element/flash (case sensitive fix for Linux)
+$srcFlash = "/var/www/adlinkfly/templates/Element/Flash";
+$dstFlash = "/var/www/adlinkfly/templates/element/flash";
+if (is_dir($srcFlash) && !is_dir($dstFlash)) {
+    exec("cp -r \"$srcFlash\" \"$dstFlash\"");
+    chown($dstFlash, 'nginx');
 }
 echo "Done";
